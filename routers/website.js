90e4accb2f31bcad7706824d1dd2893cbcd2bce3
zoubeihua <240917020@qq.com>
2017-03-23 01:20:47 +0800

    前端首页添加了遍历导航数据

--- routers/website.js
+++ routers/website.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Category = require('../models/Category');
 //首页
 router.get('/',function(req,res,next){
-    res.render('website/index',{
-        userInfo:req.userInfo
-    });
+    Category.find().then(function(categories){
+        res.render('website/index',{
+            userInfo:req.userInfo,
+            categories:categories
+        }); 
+    })
+    
 });
 //生活资讯
 router.get('/live',function(req,res,next){
